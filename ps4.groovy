1cfaa6dc5c749fa5e441e944dc2d78dc55f25ed6
cometfish <me+github@cporter.dev>
2020-01-02 16:12:44 +1100

    Add automatic polling to check status Add a double-check if the PS goes from on/standby to off - it does not respond during the transition from on to standby and vice versa

--- ps4.groovy
+++ ps4.groovy
@@ -23,11 +23,22 @@ metadata {
 preferences {
     section("URIs") {
         input "ipAddress", "text", title: "IP Address", required: true
+        input "pollInterval", "text", title: "Poll interval (minutes, or 0 for off)", required: true, defaultValue: "0"
         input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
     }
 }
 
 def updated() {
+    log.info "Settings updated"
+    
+	unschedule()
+    if(settings?.pollInterval>0 && settings?.pollInterval<=60){
+	    def pollIntervalCmd = (settings?.pollInterval)
+        Random rand = new Random(now())
+    	def randomSeconds = rand.nextInt(60)
+        def sched = "${randomSeconds} */${pollIntervalCmd} * * * ?"
+        schedule("${sched}", "refresh")
+    }
 }
 def refresh() {
     if (state.stillWaiting)
@@ -41,20 +52,32 @@ def noResponse() {
     if (state.stillWaiting)
     {
         //off
+        if (device.currentValue("power")!="off" && !state.recheckOff) {
+            //going from on to standby mode, or standby mode to on, the PS has about a 30s wait where it doesn't respond at all (looking like it's off)
+            //so try again in 35s - if it's still off, then it's definitely off
+            if (logEnable)
+                log.info "off - will confirm in 35s since it was previously on/standby";
+            state.stillWaiting = false;
+            state.recheckOff = true;
+            runIn(35, refresh);
+            return;
+        }
         if (logEnable)
             log.info "off";
         sendEvent(name: "switch", value: "off", isStateChange: true);
         sendEvent(name: "power", value: "off", isStateChange: true);
         state.stillWaiting = false;
+        state.recheckOff = false;
     }
 }
 def refreshCallback(String response) {
     state.stillWaiting = false;
+    state.recheckOff = false;
     httpResponse = new String(hubitat.helper.HexUtils.hexStringToByteArray(parseLanMessage(response).payload));
     if (httpResponse.toUpperCase().contains("HTTP/1.1 200")) {
-        //powered on
+        //on
         if (logEnable)
-            log.info "powered on";
+            log.info "on";
         sendEvent(name: "switch", value: "on", isStateChange: true);
         sendEvent(name: "power", value: "on", isStateChange: true);
     } else if (httpResponse.toUpperCase().contains("HTTP/1.1 620")) {
